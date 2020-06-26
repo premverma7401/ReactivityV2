b805d8dbb5aeaeceb7009052d0ec131dd1bc86bf
prem sager <premverma7401@gmail.com>
2020-06-26 23:01:59 +1200

    Initial setup with ValueController

---
+++ Api/Controllers/ValueController.cs
@@ -0,0 +1,49 @@
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Value>>> GetValues()
+        {
+            var items = await _context.Value.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> GetValueById(int id)
+        {
+            var items = await _context.Value.FindAsync(id);
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public void SaveValue(Value vlm)
+        {
+            var vl = new Value
+            {
+                Name = vlm.Name
+            };
+            _context.Value.Add(vl);
+            _context.SaveChanges();
+
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<Value> Value { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+                new Value
+                { Id = 1, Name = "Prem" },
+                new Value
+                { Id = 2, Name = "Sager" },
+                 new Value
+                 { Id = 3, Name = "Verma" });
+        }
+    }
+}
